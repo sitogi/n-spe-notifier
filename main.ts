33dad775e68b8592e005921f2650ed05f38fb607
sitogi <takano162@gmail.com>
2023-12-11 23:26:32 +0900

    Add Slack notification

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
 import { ProgramListResponse } from "./nhkApiTypes.ts";
-import { getCurrentDate } from "./utils.ts";
+import { getCurrentDate, sendSlackNotification } from "./utils.ts";
 
 const env = await load();
 const apiKey = env["API_KEY"];
-const currentDate = getCurrentDate();
+const slackWebhookUrl = env["SLACK_WEBHOOK_URL"];
+if (!apiKey || !slackWebhookUrl) {
+  throw new Error("No API_KEY or SLACK_WEBHOOK_URL in .env file");
+}
 
+const currentDate = getCurrentDate();
 const requestUrlWithoutKey = `https://api.nhk.or.jp/v2/pg/list/130/g1/${currentDate}.json`;
 
 console.log({ requestUrlWithoutKey });
@@ -16,4 +20,7 @@ const nhkProgramList = data.list.g1;
 
 const titleList = nhkProgramList.filter((program) => program.title.includes("ＮＨＫスペシャル"));
 
-console.log(titleList.length > 0 ? titleList[0].title : "ＮＨＫスペシャルはありません");
+await sendSlackNotification(
+  slackWebhookUrl,
+  titleList.length > 0 ? titleList[0].title : "今日は NHK スペシャルは放送しません。"
+);
