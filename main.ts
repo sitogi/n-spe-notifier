b4388fbc41c38df3c107cedce0e5e6e44aa7a86d
sitogi <takano162@gmail.com>
2023-12-11 23:31:22 +0900

    Make it works as cron job

--- main.ts
+++ main.ts
@@ -2,25 +2,27 @@ import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
 import { ProgramListResponse } from "./nhkApiTypes.ts";
 import { getCurrentDate, sendSlackNotification } from "./utils.ts";
 
-const env = await load();
-const apiKey = env["API_KEY"];
-const slackWebhookUrl = env["SLACK_WEBHOOK_URL"];
-if (!apiKey || !slackWebhookUrl) {
-  throw new Error("No API_KEY or SLACK_WEBHOOK_URL in .env file");
-}
+Deno.cron("N spe check", "0 23 * * *", async () => {
+  const env = await load();
+  const apiKey = env["API_KEY"];
+  const slackWebhookUrl = env["SLACK_WEBHOOK_URL"];
+  if (!apiKey || !slackWebhookUrl) {
+    throw new Error("No API_KEY or SLACK_WEBHOOK_URL in .env file");
+  }
 
-const currentDate = getCurrentDate();
-const requestUrlWithoutKey = `https://api.nhk.or.jp/v2/pg/list/130/g1/${currentDate}.json`;
+  const currentDate = getCurrentDate();
+  const requestUrlWithoutKey = `https://api.nhk.or.jp/v2/pg/list/130/g1/${currentDate}.json`;
 
-console.log({ requestUrlWithoutKey });
+  console.log({ requestUrlWithoutKey });
 
-const response = await fetch(`${requestUrlWithoutKey}?key=${apiKey}`);
-const data: ProgramListResponse = await response.json();
-const nhkProgramList = data.list.g1;
+  const response = await fetch(`${requestUrlWithoutKey}?key=${apiKey}`);
+  const data: ProgramListResponse = await response.json();
+  const nhkProgramList = data.list.g1;
 
-const titleList = nhkProgramList.filter((program) => program.title.includes("ＮＨＫスペシャル"));
+  const titleList = nhkProgramList.filter((program) => program.title.includes("ＮＨＫスペシャル"));
 
-await sendSlackNotification(
-  slackWebhookUrl,
-  titleList.length > 0 ? titleList[0].title : "今日は NHK スペシャルは放送しません。"
-);
+  await sendSlackNotification(
+    slackWebhookUrl,
+    titleList.length > 0 ? titleList[0].title : "今日は NHK スペシャルは放送しません。"
+  );
+});
