04d303a83cd2cf88c09c6d669f3dc57baa184843
sitogi <takano162@gmail.com>
2023-12-14 08:14:04 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { notifyNhkSpecial } from "./nhkProgramNotifier.ts";
 
-Deno.cron("N spe check", "* 23 * * *", async () => {
+Deno.cron("N spe check", "* 23 0 0 0", async () => {
   await notifyNhkSpecial();
 });
